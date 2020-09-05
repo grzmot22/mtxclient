23c84bde3614bed06a22e608f6eee4e20623e31f
Grzmot22 <grzmot22@gmail.com>
2020-09-05 03:49:38 +0100

    Fixes building

--- src/http/session.cpp
+++ src/http/session.cpp
@@ -1,4 +1,5 @@
 #include "mtxclient/http/session.hpp"
+#include <iostream>
 
 using namespace mtx::http;
 
@@ -9,38 +10,31 @@ Session::Session(boost::asio::io_service &ios,
                  RequestID id,
                  SuccessCallback on_success,
                  FailureCallback on_failure)
-  : resolver_(ios)
-  , socket(ios, ssl_ctx)
-  , host(std::move(host))
-  , port{port}
-  , id(std::move(id))
-  , on_success(std::move(on_success))
-  , on_failure(std::move(on_failure))
-  , is_shutting_down_(false)
+    : resolver_(ios), socket(ios, ssl_ctx), host(std::move(host)), port{port}, id(std::move(id)), on_success(std::move(on_success)), on_failure(std::move(on_failure)), is_shutting_down_(false)
 {
         parser.header_limit(8192);
         parser.body_limit(1 * 1024 * 1024 * 1024); // 1 GiB
 }
 
-void
-Session::on_resolve(boost::system::error_code ec,
-                    boost::asio::ip::tcp::resolver::results_type results)
+void Session::on_resolve(boost::system::error_code ec,
+                         boost::asio::ip::tcp::resolver::results_type results)
 {
-        if (ec) {
+        if (ec)
+        {
                 on_failure(id, ec);
                 return;
         }
 
         boost::asio::async_connect(
-          socket.next_layer(),
-          results,
-          std::bind(&Session::on_connect, shared_from_this(), std::placeholders::_1));
+            socket.next_layer(),
+            results,
+            std::bind(&Session::on_connect, shared_from_this(), std::placeholders::_1));
 }
 
-void
-Session::on_close(boost::system::error_code ec)
+void Session::on_close(boost::system::error_code ec)
 {
-        if (ec == boost::asio::error::eof) {
+        if (ec == boost::asio::error::eof)
+        {
                 // Rationale:
                 // http://stackoverflow.com/questions/25587403/boost-asio-ssl-async-shutdown-always-finishes-with-an-error
                 ec.assign(0, ec.category());
@@ -60,36 +54,33 @@ Session::on_close(boost::system::error_code ec)
                 std::cout << "shutdown: " << ec.message() << std::endl;
 }
 
-void
-Session::on_connect(const boost::system::error_code &ec)
+void Session::on_connect(const boost::system::error_code &ec)
 {
-        if (ec) {
+        if (ec)
+        {
                 on_failure(id, ec);
                 return;
         }
 
         // Perform the SSL handshake
         socket.async_handshake(
-          boost::asio::ssl::stream_base::client,
-          std::bind(&Session::on_handshake, shared_from_this(), std::placeholders::_1));
+            boost::asio::ssl::stream_base::client,
+            std::bind(&Session::on_handshake, shared_from_this(), std::placeholders::_1));
 }
 
-void
-Session::terminate()
+void Session::terminate()
 {
         is_shutting_down_ = true;
         shutdown();
 }
 
-void
-Session::shutdown()
+void Session::shutdown()
 {
         socket.async_shutdown(
-          std::bind(&Session::on_close, shared_from_this(), std::placeholders::_1));
+            std::bind(&Session::on_close, shared_from_this(), std::placeholders::_1));
 }
 
-void
-Session::on_request_complete()
+void Session::on_request_complete()
 {
         if (is_shutting_down_)
                 return;
@@ -100,42 +91,41 @@ Session::on_request_complete()
         shutdown();
 }
 
-void
-Session::on_handshake(const boost::system::error_code &ec)
+void Session::on_handshake(const boost::system::error_code &ec)
 {
-        if (ec) {
+        if (ec)
+        {
                 on_failure(id, ec);
                 return;
         }
 
         boost::beast::http::async_write(
-          socket,
-          request,
-          std::bind(
-            &Session::on_write, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+            socket,
+            request,
+            std::bind(
+                &Session::on_write, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
 }
 
-void
-Session::on_write(const boost::system::error_code &ec, std::size_t bytes_transferred)
+void Session::on_write(const boost::system::error_code &ec, std::size_t bytes_transferred)
 {
         boost::ignore_unused(bytes_transferred);
 
-        if (ec) {
+        if (ec)
+        {
                 on_failure(id, ec);
                 return;
         }
 
         // Receive the HTTP response
         boost::beast::http::async_read(
-          socket,
-          output_buf,
-          parser,
-          std::bind(
-            &Session::on_read, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
+            socket,
+            output_buf,
+            parser,
+            std::bind(
+                &Session::on_read, shared_from_this(), std::placeholders::_1, std::placeholders::_2));
 }
 
-void
-Session::on_read(const boost::system::error_code &ec, std::size_t bytes_transferred)
+void Session::on_read(const boost::system::error_code &ec, std::size_t bytes_transferred)
 {
         boost::ignore_unused(bytes_transferred);
 
@@ -145,11 +135,11 @@ Session::on_read(const boost::system::error_code &ec, std::size_t bytes_transfer
         on_request_complete();
 }
 
-void
-Session::run() noexcept
+void Session::run() noexcept
 {
         // Set SNI Hostname (many hosts need this to handshake successfully)
-        if (!SSL_set_tlsext_host_name(socket.native_handle(), host.c_str())) {
+        if (!SSL_set_tlsext_host_name(socket.native_handle(), host.c_str()))
+        {
                 boost::system::error_code ec{static_cast<int>(::ERR_get_error()),
                                              boost::asio::error::get_ssl_category()};
                 std::cerr << ec.message() << "\n";
